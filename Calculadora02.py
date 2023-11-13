3ab1543f60c82c3f0e368fc1fb4fc559938a0f5d
carlosgiorgio <carlosgiorgio@gmail.com>
2023-11-13 15:20:50 -0300

    Create Calculadora02.py

---
+++ Calculadora02.py
@@ -0,0 +1,43 @@
+#Função Calculadora
+
+
+def soma(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def mult(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+
+aux = True
+
+while aux:
+
+    op = int(input("Digite o operador para operação: 1 = Soma; 2 = Subtação; 3 = Multiplicação; 4 = Divisão; 0 - Fechar Calculadora."))
+
+    if op == 1:
+        num1 = float(input("Digite o primeiro número para calcular:"))
+        num2 = float(input("Digite o segundoi número para calcular:"))
+        print(soma(num1, num2))
+
+    if op == 2:
+        num1 = float(input("Digite o primeiro número para calcular:"))
+        num2 = float(input("Digite o segundoi número para calcular:"))
+        print(sub(num1, num2))
+
+    if op == 3:
+        num1 = float(input("Digite o primeiro número para calcular:"))
+        num2 = float(input("Digite o segundoi número para calcular:"))
+        print(mult(num1, num2))
+
+    if op == 4:
+        num1 = float(input("Digite o primeiro número para calcular:"))
+        num2 = float(input("Digite o segundoi número para calcular:"))
+        print(div(num1, num2))
+
+    if op == 0:
+        break
+
+
+
