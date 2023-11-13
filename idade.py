ec36518400194f9294e39c125d619e9b8951b300
carlosgiorgio <carlosgiorgio@gmail.com>
2023-11-13 15:52:41 -0300

    Update idade.py

--- idade.py
+++ idade.py
@@ -1,1 +1,6 @@
+nome = input("Digite seu nome:")
+data_nascimento = int(input("Se você nasceu entre 1922 e 2021, então digite sua data de nascimento:"))
 
+idade = 2022 - data_nascimento
+
+print(nome," sua idade é: ", idade)
