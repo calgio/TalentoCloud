00580e0b024d5d134f3a1c8e1b03bdced930fb75
carlosgiorgio <carlosgiorgio@gmail.com>
2023-11-13 16:15:51 -0300

    Update idade.py

--- idade.py
+++ idade.py
@@ -1,6 +1,12 @@
-nome = input("Digite seu nome:")
-data_nascimento = int(input("Se você nasceu entre 1922 e 2021, então digite sua data de nascimento:"))
+numeroCorreto = False
 
-idade = 2022 - data_nascimento
-
-print(nome," sua idade é: ", idade)
+while (numeroCorreto == False):
+    try:
+        nome = input("Digite seu nome:")
+        data_nascimento = int(input("Se você nasceu entre 1922 e 2021, então digite sua data de nascimento:"))
+        if data_nascimento > 1922:
+            idade = 2022 - data_nascimento
+            numeroCorreto = True
+            print(nome,"sua idade é:", idade)
+    except:
+        print("Não foi digitado um número.")
